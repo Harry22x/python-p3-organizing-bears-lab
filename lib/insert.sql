0c294c253a405d1d1e691f9712a4f1521301f3ee
Sitoh7 <ha.wakhule@gmail.com>
2024-12-07 16:14:56 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',23,'M','Black','?',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',23,'M','Black','?',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',23,'M','Black','?',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',23,'M','Black','?',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',23,'M','Black','?',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',23,'M','Black','?',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',23,'M','Black','?',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (null,23,'M','Black','?',True);
